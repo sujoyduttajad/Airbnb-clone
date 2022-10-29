7470f5ebf1f1312a8bf92bf4b39657aa7a20c7b0
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-29 16:17:34 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,28 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-        <h1>Header</h1>
+      <h1>Header</h1>
 
-        {/* Left Container */}
-        <div>
-            <Image src="https://links.papareact.com/qd3" />
-        </div>
+      {/* Left Container */}
+      <div>
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
 
-        {/* middle Container */}
-        <div></div>
+      {/* middle Container */}
+      <div></div>
 
-        {/* Right Container */}
-        <div></div>
+      {/* Right Container */}
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
