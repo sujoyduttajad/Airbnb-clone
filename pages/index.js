1203f659dea91069d2f1c81abc68ff581936bd9c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-29 22:56:42 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 // import Image from 'next/image'
 
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
       
       <Header />
-     
+      <Banner />
     </div>
   );
 }
