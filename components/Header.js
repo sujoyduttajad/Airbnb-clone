f184f307fdc0a18ca6d2b91487d04f8997a8e6ea
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-29 23:04:36 +0530

    Made the sticky work

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ import {
 
 const Header = () => {
   return (
-    <div className="sticky top-0 z-100 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+    <div className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left Container */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
