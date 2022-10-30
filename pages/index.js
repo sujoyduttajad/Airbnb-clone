fda36ca53125d52da3dc62161396079fd89aac53
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-30 18:20:35 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,17 @@ export default function Home() {
       <Header />
       <Banner />
 
-      <main className="max-w-7xl mx-auto">
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
         </section>
       </main>
     </div>
+  );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
   );
 }
