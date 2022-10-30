8ef39890a5b0d0063584a49823f6c9b27e703e2b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-30 20:27:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,14 +35,16 @@ export default function Home({ exploreData }) {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          {exploreData.nearbyCities?.map((item, index) => (
-            <SmallCard
-              key={index}
-              img={item.img}
-              location={item.location}
-              distance={item.distance}
-            />
-          ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData.nearbyCities?.map((item, index) => (
+              <SmallCard
+                key={index}
+                img={item.img}
+                location={item.location}
+                distance={item.distance}
+              />
+            ))}
+          </div>
         </section>
       </main>
     </div>
