e9e945930ed9f2675efdf1c3f9140a5acf98a5e9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-30 20:19:15 +0530

    Update SmallCard.js

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import Image from "next/image";
 
 const SmallCard = ({ img, location, distance }) => {
   return (
-    <div>SmallCard</div>
-  )
-}
+    <div className="flex items-center">
+      {/* Left container */}
+      <div className="relative h-16 w-16">
+        <Image src={img} layout="fill" objectFit="" className="rounded-lg" />
+      </div>
+      {/* Right container */}
+      <div>
+        <h2>{location}</h2>
+        <h3>{distance}</h3>
+      </div>
+    </div>
+  );
+};
 
-export default SmallCard
+export default SmallCard;
