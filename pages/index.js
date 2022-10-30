03f7c0412fedc6b7ff1f7f34ff2f4f9d28d2fe15
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-30 20:28:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home({ exploreData }) {
       <Banner />
 
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
-        <section className="pt-6">
+        <section className="pt-6 mb-8">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {exploreData.nearbyCities?.map((item, index) => (
