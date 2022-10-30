90070f2deeb5f56678c3f16d74324d87da3a1f58
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-31 02:07:19 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,18 +6,22 @@ import {
   SearchIcon,
   UserCircleIcon,
 } from "@heroicons/react/solid";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left Container */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
-        <Image
-          src="https://links.papareact.com/qd3"
-          layout="fill"
-          objectFit="contain"
-          objectPosition="left"
-        />
+      <div className="relative flex items-center justify-start h-10 my-auto">
+        <Link href="/">
+          <Image
+            src="https://links.papareact.com/qd3"
+            layout="fill"
+            objectFit="contain"
+            objectPosition="left"
+            className="cursor-pointer"
+          />
+        </Link>
       </div>
 
       {/* Middle Container - Search bar */}
@@ -38,7 +42,9 @@ const Header = () => {
 
       {/* Right Container */}
       <div className="flex items-center justify-end space-x-4 text-gray-500 ">
-        <p className="hidden md:inline cursor-pointer hover:underline">Become a host</p>
+        <p className="hidden md:inline cursor-pointer hover:underline">
+          Become a host
+        </p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
           <MenuIcon className="h-6 cursor-pointer" />
