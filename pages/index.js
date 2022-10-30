9f6c28e394a27be9a672fd38f18378cbd250bbbb
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-30 19:43:34 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
-// import Image from 'next/image'
 
 export default function Home({ exploreData }) {
   console.log(exploreData);
@@ -19,9 +18,9 @@ export default function Home({ exploreData }) {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          {exploreData !== undefined && exploreData.content.detailed.map((item) => (
+          {/* {exploreData !== undefined && exploreData.content.detailed.map((item) => (
             <h1>{item.city}</h1>
-          ))}
+          ))} */}
         </section>
       </main>
     </div>
@@ -30,21 +29,14 @@ export default function Home({ exploreData }) {
 
 // This is running inside a server
 export async function getStaticProps() {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': process.env.API_KEY,
-      'X-RapidAPI-Host': 'mashvisor-api.p.rapidapi.com'
-    }
-  };
-  const exploreData = await fetch(
-    "https://mashvisor-api.p.rapidapi.com/rental-rates?state=CA&source=airbnb&city=Los%20Angeles&zip_code=90291", options
-  ).then(res => res.json())
-  .catch(err => console.error(err));
-
+  const exploreData = await fetch("../cities.json").then(
+    (res) => res.json()
+  );
+ 
   return {
     props: {
-      exploreData,
-    },
-  };
+      exploreData
+    }
+  }
 }
+
