ea9af0d27875481bf1666add8ac2753029ea9cd4
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-30 20:23:06 +0530

    Update SmallCard.js

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 const SmallCard = ({ img, location, distance }) => {
   return (
-    <div className="flex items-center">
+    <div 
+        className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer 
+        hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out"
+    >
       {/* Left container */}
       <div className="relative h-16 w-16">
         <Image src={img} layout="fill" objectFit="" className="rounded-lg" />
