5dc03567822cc9852c05b287805444b71ceaf83b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-31 17:21:04 +0530

    Update MediumCard.js

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import Image from "next/image";
 
 const MediumCard = ({ img, title }) => {
   return (
-    <div>MediumCard</div>
+    <div className="relative h-80 w-80">
+        <Image 
+            src={img}
+            layout="fill"
+
+        />
+    </div>
   )
 }
 
