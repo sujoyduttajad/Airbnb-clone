bd79c8b1bb617c1777e4c9c22402d784945ba7b8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-02 15:32:00 +0530

    Update LargeCard.js

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -3,22 +3,24 @@ import Image from "next/image";
 const LargeCard = ({ img, title, description, buttonText }) => {
   return (
     <section className="relative py-16 cursor-pointer">
-        <div className="relative h-96 min-w-[300px]">
-            <Image 
-                src={img}
-                alt={title}
-                layout="fill"
-                objectFit="cover"  
-                className="rounded-2xl"
-            />
-        </div>
-        <div>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <button>{buttonText}</button>
-        </div>
-    </section>   
-  )
-}
+      <div className="relative h-96 min-w-[300px]">
+        <Image
+          src={img}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-2xl"
+        />
+      </div>
+      <div className="absolute top-32 left-12">
+        <h3 className="text-4xl mb-3 w-64">{title}</h3>
+        <p className="font-normal text-gray-600">{description}</p>
+        <button className="text-sm text-white bg-gray-700 px-4 py-2 rounded-lg mt-5">
+          {buttonText}
+        </button>
+      </div>
+    </section>
+  );
+};
 
-export default LargeCard
+export default LargeCard;
