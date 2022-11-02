67b6876bda18bc68ef4fc1297ed669e40b138d9e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-02 13:12:07 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import fsPromises from "fs/promises";
 import path from "path";
 import SmallCard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
 
 // This is running inside a server
 export async function getStaticProps() {
@@ -32,6 +33,7 @@ export default function Home({ exploreData }) {
       <Banner />
 
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        {/* Small Card */}
         <section className="pt-6 mb-8">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
@@ -45,12 +47,19 @@ export default function Home({ exploreData }) {
             ))}
           </div>
         </section>
+        {/* Medium Card */}
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {exploreData.getaways?.map((item, index) => (
               <MediumCard key={index} img={item.img} title={item.title} />
             ))}
+          </div>
+        </section>
+        {/* Large Card */}
+        <section>
+          <div>
+            <LargeCard />
           </div>
         </section>
       </main>
