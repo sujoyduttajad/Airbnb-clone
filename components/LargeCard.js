e187d2c14bca4e4357fa8eb00d02dc5978e1f71e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-02 15:39:56 +0530

    Update LargeCard.js

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -15,7 +15,7 @@ const LargeCard = ({ img, title, description, buttonText }) => {
       <div className="absolute top-32 left-12">
         <h3 className="text-4xl mb-3 w-64">{title}</h3>
         <p className="font-normal text-gray-600">{description}</p>
-        <button className="text-sm text-white bg-gray-700 px-4 py-2 rounded-lg mt-5">
+        <button className="text-sm text-white bg-gray-800 px-4 py-2 rounded-lg mt-5">
           {buttonText}
         </button>
       </div>
