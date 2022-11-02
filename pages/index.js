2ed035fdbff13b4a8df888df66ad380834a4ed69
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-02 13:07:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home({ exploreData }) {
         </section>
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-          <div className="flex space-x-3 overflow-scroll scrollbar-hide">
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {exploreData.getaways?.map((item, index) => (
               <MediumCard key={index} img={item.img} title={item.title} />
             ))}
