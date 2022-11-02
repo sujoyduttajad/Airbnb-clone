4558869dbd9a3a2dd1a82a945a3d4e56068a90bc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-02 15:45:13 +0530

    Update SmallCard.js

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -4,7 +4,7 @@ const SmallCard = ({ img, location, distance }) => {
   return (
     <div 
         className="flex items-center m-2 mt-5 pl-3 py-2 space-x-4 rounded-xl cursor-pointer 
-        hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out"
+        hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out no-flickr"
     >
       {/* Left container */}
       <div className="relative h-16 w-16">
@@ -12,8 +12,8 @@ const SmallCard = ({ img, location, distance }) => {
       </div>
       {/* Right container */}
       <div>
-        <h2 className="font-medium">{location}</h2>
-        <h3 className="text-gray-500">{distance}</h3>
+        <h2 className="no-flickr font-medium">{location}</h2>
+        <h3 className="text-gray-500 no-flickr">{distance}</h3>
       </div>
     </div>
   );
