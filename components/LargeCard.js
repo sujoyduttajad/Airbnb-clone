41e1849f3079c679d9986d272614dca4d7edac47
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-02 14:47:23 +0530

    Update LargeCard.js

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -2,16 +2,21 @@ import Image from "next/image";
 
 const LargeCard = ({ img, title, description, buttonText }) => {
   return (
-    <section>
+    <section className="relative py-16 cursor-pointer">
         <div className="relative h-96 min-w-[300px]">
             <Image 
                 src={img}
                 alt={title}
                 layout="fill"
                 objectFit="cover"  
+                className="rounded-2xl"
             />
         </div>
-        <div></div>
+        <div>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <button>{buttonText}</button>
+        </div>
     </section>   
   )
 }
