e7aad2d1e6b4183ecf8112333fbe51dfe7c61e80
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-02 16:18:22 +0530

    Update LargeCard.js

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const LargeCard = ({ img, title, description, buttonText }) => {
   return (
-    <section className="relative py-16 cursor-pointer">
+    <section className="relative my-16">
       <div className="relative h-96 min-w-[300px]">
         <Image
           src={img}
@@ -12,12 +12,12 @@ const LargeCard = ({ img, title, description, buttonText }) => {
           className="rounded-2xl"
         />
       </div>
-      <div className="absolute top-32 left-12">
+      <div className="absolute top-11 left-12">
         <h3 className="text-4xl mb-3 w-64">{title}</h3>
         <p className="font-normal text-gray-600">{description}</p>
         <button 
-            className="text-sm text-white bg-gray-800 px-4 py-2 
-            rounded-lg mt-5 active:scale-90 transition duration-150"
+            className="cursor-pointer text-sm text-white bg-gray-800 
+            px-4 py-2 rounded-lg mt-5 active:scale-90 transition duration-150"
         >
           {buttonText}
         </button>
