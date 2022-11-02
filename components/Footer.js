2ecae93d06d89920a9adc215033505487bd4edaf
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-02 16:18:24 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer 
+    className='grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 
+        bg-gray-100 text-gray-600 '
+    >
         <div className='space-y-4 text-xs text-gray-800'>
             <h5 className='font-bold'>AIRBNB</h5>
             <p>How Airbnb works</p>
