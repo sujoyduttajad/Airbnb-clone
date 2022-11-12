d3be4de56da93932b32992e494430e96759ef09d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-12 16:53:46 +0530

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -1,10 +1,12 @@
 import Header from "../components/Header"
-
+import Footer from "../components/Footer"
 
 function Search() {
   return (
     <div>
         <Header />
+        <div></div>
+        <Footer />
     </div>
   )
 }
