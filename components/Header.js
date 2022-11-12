b9145f34badc894f13c358ce69f160496101d4ed
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-12 19:53:07 +0530

    Replaced Link with useRouter

--- components/Header.js
+++ components/Header.js
@@ -7,16 +7,18 @@ import {
   UserCircleIcon,
   UsersIcon,
 } from "@heroicons/react/solid";
-import Link from "next/link";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/router";
 
 const Header = () => {
   const [searchInput, setSearchInput] = useState();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoOfGuests] = useState(1);
+
+  const router = useRouter();
 
   const selectionRange = {
     startDate: startDate,
@@ -36,8 +38,10 @@ const Header = () => {
   return (
     <div className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left Container */}
-      <div className="relative flex items-center justify-start h-10 my-auto">
-        <Link href="/">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center justify-start h-10 my-auto"
+      >
           <Image
             src="https://links.papareact.com/qd3"
             layout="fill"
@@ -45,13 +49,14 @@ const Header = () => {
             objectPosition="left"
             className="cursor-pointer"
           />
-        </Link>
       </div>
 
       {/* Middle Container - Search bar */}
       <div
         className={`flex items-center justify-between md:border-2 rounded-full
-        py-2 md:shadow-sm hover:border-red-400 ${searchInput ? 'border-red-400' : ''}`}
+        py-2 md:shadow-sm hover:border-red-400 ${
+          searchInput ? "border-red-400" : ""
+        }`}
       >
         <input
           className="flex-grow pl-5 bg-transparent text-sm text-gray-500 
@@ -114,7 +119,7 @@ const Header = () => {
             >
               Cancel
             </button>
-            <button 
+            <button
               className="flex-grow hover:bg-gray-100 py-2 rounded-full 
                text-red-400 text-xl transition duration-450 user-select-none"
             >
