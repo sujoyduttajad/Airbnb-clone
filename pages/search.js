8a6f3b0ebcd9014342cf9dc52d806459c6d7351a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-12 18:12:19 +0530

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -10,7 +10,7 @@ function Search() {
           <p className="text-xs">300+ Stays for 5 number of days</p>
           <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
           <div className="hidden lg:inline-flex">
-            <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg 
+            <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg font-medium
             active:scale-95 active:bg-red-400 active:text-white transition transform duration-100">
               Cancellation Flexibility
             </p>
