2b907a5dfc4cdbd86cc87cb89933c2ba643e4c70
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-12 23:47:28 +0530

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -10,11 +10,19 @@ function Search() {
           <p className="text-xs">300+ Stays for 5 number of days</p>
           <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
-            <button className="filter-button user-select-none">Cancellation Flexibility</button>
-            <button className="filter-button user-select-none">Type of Place</button>
+            <button className="filter-button user-select-none">
+              Cancellation Flexibility
+            </button>
+            <button className="filter-button user-select-none">
+              Type of Place
+            </button>
             <button className="filter-button user-select-none">Price</button>
-            <button className="filter-button user-select-none">Rooms and Beds</button>
-            <button className="filter-button user-select-none">More filters</button>
+            <button className="filter-button user-select-none">
+              Rooms and Beds
+            </button>
+            <button className="filter-button user-select-none">
+              More filters
+            </button>
           </div>
         </section>
       </main>
