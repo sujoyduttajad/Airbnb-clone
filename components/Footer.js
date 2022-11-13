9a2b262a71f9b55873108ce48bd1dd9ffec0dcbc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-13 11:41:11 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -13,44 +13,49 @@ const Footer = () => {
         lg:space-y-0 space-y-10 lg:px-32 lg:py-14 px-10 py-12 bg-gray-100 text-gray-600"
       >
         <div className="space-y-4 text-xs text-gray-800 pr-5">
-          <h5 className="text-gray-600 font-semibold capitalize text-lg tracking-wide">Airbnb</h5>
-          <p>How Airbnb works</p>
-          <p>Newsroom</p>
-          <p>Investors</p>
-          <p>Careers</p>
-          <p>Airbnb Plus</p>
+          <h5 className="text-gray-600 font-semibold capitalize text-lg tracking-wide">
+            Airbnb
+          </h5>
+          <a>How Airbnb works</a>
+          <a>Newsroom</a>
+          <a>Investors</a>
+          <a>Careers</a>
+          <a>Airbnb Plus</a>
+          <a href="https://github.com/sujoyduttajad" target="_blank">
+            My Github Profile
+          </a>
         </div>
         <div className="space-y-4 text-xs text-gray-800 pr-5">
           <h5 className="text-gray-600 font-semibold capitalize text-lg tracking-wide">
             Community
           </h5>
-          <p>Accessibility</p>
-          <p>Visit our community forum</p>
-          <p>Airbnb.org</p>
-          <p>Support Afgan refugees</p>
-          <p>Combat discrimination</p>
+          <a>Accessibility</a>
+          <a>Visit our community forum</a>
+          <a>Airbnb.org</a>
+          <a>Support Afgan refugees</a>
+          <a>Combat discrimination</a>
         </div>
         <div className="space-y-4 text-xs text-gray-800 pr-5">
           <h5 className="text-gray-600 font-semibold capitalize text-lg tracking-wide">
             Hosting
           </h5>
-          <p>Try Hosting</p>
-          <p>AirCover for Hosts</p>
-          <p>Explore hosting resources</p>
-          <p>How to host responsibly</p>
-          <p>Privacy Policy</p>
-          <p>Terms and Conditions</p>
+          <a>Try Hosting</a>
+          <a>AirCover for Hosts</a>
+          <a>Explore hosting resources</a>
+          <a>How to host responsibly</a>
+          <a>Privacy Policy</a>
+          <a>Terms and Conditions</a>
         </div>
         <div className="space-y-4 text-xs text-gray-800">
           <h5 className="text-gray-600 font-semibold capitalize text-lg tracking-wide">
             Support
           </h5>
-          <p>Help Centre</p>
-          <p>AirCover</p>
-          <p>Safety Information</p>
-          <p>Supporting people with disabilities</p>
-          <p>Cancellation options</p>
-          <p>Report a neighbourhood concern</p>
+          <a>Help Centre</a>
+          <a>AirCover</a>
+          <a>Safety Information</a>
+          <a>Supporting people with disabilities</a>
+          <a>Cancellation options</a>
+          <a>Report a neighbourhood concern</a>
         </div>
       </section>
       <div className="py-4 pt-10 bg-gray-100 text-gray-600 flex justify-center items-center w-full">
