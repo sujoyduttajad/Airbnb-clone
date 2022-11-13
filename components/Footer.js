86a1bf688f81ec2f71af1a494b2a50a8d13b015c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-13 12:43:51 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -7,10 +7,10 @@ const currYear = () => {
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="bg-gray-100 px-10">
       <section
-        className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 sm:space-y-0 md:space-y-0 
-        lg:space-y-0 space-y-10 lg:px-32 lg:py-14 px-10 py-12 bg-gray-100 text-gray-600"
+        className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 sm:space-y-0 md:space-y-0 border-b
+        lg:space-y-0 space-y-10 lg:px-32 lg:py-14 mx-2 py-12 bg-gray-100 text-gray-600"
       >
         <div className="space-y-4 text-xs text-gray-800 pr-5">
           <h5 className="text-gray-600 font-semibold capitalize text-lg tracking-wide">
@@ -167,8 +167,9 @@ const Footer = () => {
           </p>
         </div>
       </section>
-      <div className="py-4 pt-10 bg-gray-100 text-gray-600 flex justify-center items-center w-full">
-        <p>© {currYear()} Airbnb Inc. </p>
+      <div className="py-4 pt-10 bg-gray-100 text-gray-600 flex flex-wrap justify-around items-center w-full">
+        <p>© {currYear()} Airbnb, Inc. </p>
+        <p className="font-semibold">Developed by <span className="text-red-400">Sujoy Dutta</span></p>
       </div>
     </footer>
   );
