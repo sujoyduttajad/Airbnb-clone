196066a661faef11fb3a17330f7d0d0bf0945700
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-13 19:26:38 +0530

    Update InfoCard.js

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -12,16 +12,18 @@ const InfoCard = ({
   total,
 }) => {
   return (
-    <div>
+    <div className="flex">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
-        <Image src={img} layout="fill" objectFit="cover" />
+        <Image src={img} layout="fill" objectFit="cover" priority={true} />
       </div>
 
       <div className="flex flex-col flex-grow pl-5">
         <div className="flex justify-between">
-            <p>{location}</p>
-            <HeartIcon className="h-7 cursor-pointer" />
+          <p>{location}</p>
+          <HeartIcon className="h-7 cursor-pointer" />
         </div>
+
+        <h4>{title}</h4>
       </div>
     </div>
   );
