abacdb55b01a5dca8216fe761a9494b6f4c940f1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-13 19:12:30 +0530

    Update InfoCard.js

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const InfoCard = () => {
+const InfoCard = ({
+  img,
+  title,
+  location,
+  description,
+  star,
+  price,
+  total,
+}) => {
   return (
-    <div>InfoCard</div>
-  )
-}
+    <div>
+      <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
+        <Image src={img} layout="fill" objectFit="cover" />
+      </div>
+    </div>
+  );
+};
 
-export default InfoCard
+export default InfoCard;
