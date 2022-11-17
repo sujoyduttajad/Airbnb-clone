d1bd519a610695fe7245b16d655acd915cfec580
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-17 14:02:01 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -82,7 +82,7 @@ const Header = ({ placeholder }) => {
       {/* Middle Container - Search bar */}
       <div
         className={`flex items-center justify-between md:border-2 rounded-full
-        py-2 md:shadow-sm hover:border-red-400 ${
+        py-2 shadow-sm hover:border-red-400 ${
           searchInput ? "border-red-400" : ""
         }`}
       >
@@ -107,7 +107,9 @@ const Header = ({ placeholder }) => {
         >
           Become a host
         </p>
-        <GlobeAltIcon className="h-6 cursor-pointer hover:bg-gray-100 rounded-full" />
+        <div className="md:flex lg:flex hidden">
+          <GlobeAltIcon className="h-6 cursor-pointer hover:bg-gray-100 rounded-full" />
+        </div>
         <div
           className="flex items-center space-x-2 border-2 p-2 rounded-full hover:shadow-xl 
         transition duration-150 cursor-pointer"
