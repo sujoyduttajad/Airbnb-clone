e6611bcb2286c0701a13bce928653ba97b37a489
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-19 20:04:05 +0530

    Update MapboxMap.js

--- components/MapboxMap.js
+++ components/MapboxMap.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Map from "react-map-gl";
 
 const MapboxMap = () => {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 11,
+  });
+
   return (
     <Map
       mapStyle="mapbox://styles/sujoy11/clagxis0m000b15nn826z74bn"
       mapboxAccessToken={process.env.mapbox_key}
+      {...viewport}
     ></Map>
   );
 };
