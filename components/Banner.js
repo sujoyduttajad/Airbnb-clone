c4a3fc817fbb3ff31edc47dfd5f761ea20be0ba3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-23 16:22:31 +0530

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -9,6 +9,7 @@ const Banner = () => {
         layout="fill"
         objectFit="cover"
         priority={true}
+        alt="A beautiful Banner"
       />
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
@@ -16,7 +17,7 @@ const Banner = () => {
           className="text-purple-500 bg-white px-10 py-4 shadow-md 
           rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150"
         >
-          I'm flexible
+          I am flexible
         </button>
       </div>
     </div>
