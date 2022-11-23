a6c7d46c9ee556cba9bd518a1c0bf4047d4f48f9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-23 13:48:28 +0530

    Update MapboxMap.js

--- components/MapboxMap.js
+++ components/MapboxMap.js
@@ -4,6 +4,8 @@ import { getCenter } from "geolib";
 import { ThumbIcon } from "./SocialIcons";
 
 const MapboxMap = ({ data }) => {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
   const coordinates = data.map((result) => ({
     longitude: result.long,
     latitude: result.lat,
@@ -18,14 +20,14 @@ const MapboxMap = ({ data }) => {
   });
 
   const mapRef = useRef();
- 
+
   return (
     <Map
       ref={mapRef}
       mapStyle="mapbox://styles/sujoy11/clagxis0m000b15nn826z74bn"
       mapboxAccessToken={process.env.mapbox_key}
       onMove={(evt) => setViewport(evt.viewState)}
-      style={{width: 600, height: "100%", cursor: "auto"}}
+      style={{ width: 600, height: "100%", cursor: "auto" }}
       dragPan={true}
       boxZoom={true}
       {...viewport}
@@ -38,8 +40,27 @@ const MapboxMap = ({ data }) => {
             offsetLeft={-20}
             offsetTop={-10}
           >
-            <p className="cursor-pointer text-2xl m-0 animate-bounce">
-              <ThumbIcon />
+            <p
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl m-0 animate-bounce"
+              aria-label="push-pin"
+            >
+              {/* <ThumbIcon /> */}
+              📌
+              {/* Popup renders onClick of the Marker */}
+              {selectedLocation.long === result.long ? (
+                <Popup
+                  closeOnClick={true}
+                  latitude={result.lat}
+                  longitude={result.long}
+                  anchor="center"
+                  onClose={() => setSelectedLocation({})}
+                >
+                  {result.title}
+                </Popup>
+              ) : (
+                false
+              )}
             </p>
           </Marker>
         </div>
