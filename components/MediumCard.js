a2a483eacc4c65ce4d822dcdc4c28287d21231c0
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-23 16:28:57 +0530

    Update MediumCard.js

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -4,7 +4,7 @@ const MediumCard = ({ img, title }) => {
   return (
     <div className="no-flickr cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
       <div className="relative h-80 w-80">
-        <Image src={img} layout="fill" className="rounded-xl" />
+        <Image src={img} layout="fill" className="rounded-xl" alt="Type of Airbnb Image" />
       </div>
       <h3 className="no-flickr text-2xl mt-3">{title}</h3>
     </div>
