5b019a4224d1f67d67131fbe45531c775ee4e0b1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-03 11:04:25 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 import("tailwindcss").Config;
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+// const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        raleway: ['Raleway', ...defaultTheme.fontFamily.sans]
-      },
-    },
-  },
   plugins: [require("tailwind-scrollbar-hide")],
 };
