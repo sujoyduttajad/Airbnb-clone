584dbae54b3e525bf967d61ceb1bef82f7c6aa58
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-03 12:14:25 +0530

    Create Dropdown.js

---
+++ components/Dropdown.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Dropdown = () => {
+  return (
+    <div>
+      <div
+        id="dropdownInformation"
+        className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+      >
+        <div className="py-3 px-4 text-sm text-gray-900 dark:text-white">
+          <div>Bonnie Green</div>
+          <div className="font-medium truncate">name@flowbite.com</div>
+        </div>
+        <ul
+          className="py-1 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownInformationButton"
+        >
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Settings
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Earnings
+            </a>
+          </li>
+        </ul>
+        <div className="py-1">
+          <a
+            href="#"
+            className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+          >
+            Sign out
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
